ccfb89a9cfe044ed8baf38c44d0d25aee1e2ce05
Mohamed Soussi <mohameddsoussi@gmail.com>
2017-06-10 14:05:10 -0400

    securing access to agreement signup flow

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -6,36 +6,82 @@ import './PageLayout.scss';
 class PageLayout extends React.Component {
     constructor(...args) {
         super(...args);
+
+        this.tok = this.props.location.query.tok;
+        this.state = {
+            showChildren: false
+        }
+    }
+
+    shouldComponentUpdate(){
+        return true
+    }
+
+    componentDidMount(){
+        this.validate(this.tok);
+    }
+
+    validate(token){
+        fetch("https://daycay.co:8080/validate_agreement_token?token=" + token)
+            .then((function(response){
+                if(response.ok){
+                    return response.json()
+                } 
+            }))
+            .then(function(j){
+                if(j.status == "success"){
+                    this.setState({showChildren: true})
+                }
+            }.bind(this))
+            .catch((err)=>{
+                console.error(err)
+            })
     }
 
     render() {
-        return (
-            <div className='container text-center'>
-                <div className='nav-back'>
-                    <a href="http://www.google.com">
-                        Go To Management Software Demo
-                        <FontAwesomeIcon icon='long-arrow-right' />
-                    </a>
-                </div>
-                <div className='row'>
-                    <div className='col-md-12'>
-                        <div className='main'>
-                            <h3 className='main-title'>Daycation Program Setup</h3>
-                            <div className='step-links'>
-                                <IndexLink to='/' activeClassName='page-layout__nav-item--active'>1<br />Hotel Info</IndexLink>
-                                <Link to='/day-passes' activeClassName='page-layout__nav-item--active'>2<br />Day Passes</Link>
-                                <Link to='/cabanas' activeClassName='page-layout__nav-item--active'>3<br />Cabanas</Link>
-                                <Link to='/agreement' activeClassName='page-layout__nav-item--active'>4<br />Agreement</Link>
-                            </div>
+        if(this.state.showChildren){
+            return (
+                <div className='container text-center'>
+                    <div className='nav-back'>
+                        <a href="http://www.google.com">
+                            Go To Management Software Demo
+                            <FontAwesomeIcon icon='long-arrow-right' />
+                        </a>
+                    </div>
+                    <div className='row'>
+                        <div className='col-md-12'>
+                            <div className='main'>
+                                <h3 className='main-title'>Daycation Program Setup</h3>
+                                <div className='step-links'>
+                                    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>1<br />Hotel Info</IndexLink>
+                                    <Link to='/day-passes' activeClassName='page-layout__nav-item--active'>2<br />Day Passes</Link>
+                                    <Link to='/cabanas' activeClassName='page-layout__nav-item--active'>3<br />Cabanas</Link>
+                                    <Link to='/agreement' activeClassName='page-layout__nav-item--active'>4<br />Agreement</Link>
+                                </div>
 
-                            <div className='page-layout__viewport'>
-                                {this.props.children}
+                                <div className='page-layout__viewport'>
+                                    {this.props.children}
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        } else {
+            return(
+                <div className='DaycationView'>
+                    <img className='DaycationLogo' src='/imgs/DaycationSymbol.RGB.Orange.png' alt='DaycationLogo_Orange'/>
+                    <div className='DaycationAlert'>
+                        <h3 className='DaycationAlert__Header'>Access Forbidden</h3>
+                        <div className='DaycationAlert__EmailLink'>
+                            Send us an email at <a className='DaycationLink' href='mailto:matt@daycationapp.com'>matt@daycationapp.com</a>&nbsp; 
+                            if you'd like to partner with Daycation.
+                        </div>
+                    </div>
+                </div>
+            )
+        }
+        
     }
 }
 
